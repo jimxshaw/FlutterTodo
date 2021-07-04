c8100abb41b63cd2fb2477137d7cd04bc6543377
Jim Shaw <jimxshaw@gmail.com>
2021-07-03 19:50:06 -0500

    Add boilerplate tasks screen widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertodo/screens/tasks_screen.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
