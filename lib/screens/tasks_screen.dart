b647e41d190358e3815dec7fd024ce8f0cc111bc
Jim Shaw <jimxshaw@gmail.com>
2021-07-04 12:54:52 -0500

    Update text.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -11,6 +11,7 @@ class TasksScreen extends StatelessWidget {
         padding:
             EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CircleAvatar(
               child: Icon(
