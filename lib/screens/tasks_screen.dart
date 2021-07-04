45826a45f556f5863455b30a33a1b0e2d9596ce6
Jim Shaw <jimxshaw@gmail.com>
2021-07-04 13:16:45 -0500

    Update UI.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,12 +7,18 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orangeAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orangeAccent,
+        child: Icon(
+          Icons.add,
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding:
-                EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
