a781b202e6a6babe84622f497e42e372a25eb739
Jim Shaw <jimxshaw@gmail.com>
2021-07-03 20:02:03 -0500

    Update CircleAvatar.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,6 +5,25 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent,
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                color: Colors.orangeAccent,
+                size: 30.0,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            Text('FlutterToDo'),
+          ],
+        ),
+      ),
+    );
   }
 }
