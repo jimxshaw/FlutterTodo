b41fa2dccb677090638f4a10ea40eedcba60d3d6
Jim Shaw <jimxshaw@gmail.com>
2021-07-14 22:10:41 -0500

    Turn TasksScreen widget back to stateless.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertodo/models/task.dart';
 import 'package:fluttertodo/models/task_data.dart';
 import 'package:fluttertodo/screens/add_task_screen.dart';
 import 'package:fluttertodo/widgets/tasks_list.dart';
 import 'package:provider/provider.dart';
 
-class TasksScreen extends StatefulWidget {
-  @override
-  _TasksScreenState createState() => _TasksScreenState();
-}
-
-class _TasksScreenState extends State<TasksScreen> {
+class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
