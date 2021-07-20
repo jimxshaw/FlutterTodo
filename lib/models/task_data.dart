66359b47a71782d779b4a08ecab6685abd27f527
Jim Shaw <jimxshaw@gmail.com>
2021-07-19 22:14:06 -0500

    Change tasks list field to private.

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -3,19 +3,19 @@ import 'package:flutter/foundation.dart';
 import 'package:fluttertodo/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: "Buy bread"),
     Task(name: "Clean house"),
     Task(name: "Do laundry"),
   ];
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String newTaskTitle) {
     final task = new Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 }
